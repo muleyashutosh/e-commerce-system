8eb0b6b639ede702deb9d8fedb578382d473f74b
Ashutosh Muley <muleyashutosh@gmail.com>
2020-10-23 20:40:18 +0530

    Login Page

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+from configparser import Error
 from os import terminal_size
 from flask import Flask, render_template, request, session, url_for
+from mysql.connector import Error
 from werkzeug.utils import redirect
 from workbench import Workbench
 import os
 from dotenv import load_dotenv
+# from webui import WebUI
 
 
 load_dotenv()
@@ -11,6 +14,7 @@ mysql_pwd = os.getenv('mySQL_muleyashutosh_password')
 
 
 myapp = Flask(__name__)
+# ui = WebUI(myapp, debug= True)
 myapp.secret_key = 'ABCDEF'
 
 @myapp.route('/', methods = ['GET', 'POST'])
@@ -19,14 +23,14 @@ def Hello_World():
         payload = request.form
         # print(payload)
         dB = Workbench(password = mysql_pwd)
-
+        login_status = None
         if('login' in payload):
             whereClause = dict([x for x in payload.items() if 'login' not in x])
             # print(userDat)
             userData = dB.select_from('users', where_clause = whereClause)
             # print(userData)
             if(userData):
-                login_status = True
+                login_status = True 
                 if payload['email'] not in session:
                     session['email'] = payload['email']
                     return redirect(url_for('home'))
@@ -38,8 +42,10 @@ def Hello_World():
         else:
             values = dict([x for x in payload.items() if 'signup' not in x])
             # print(values)
-            dB.insert_into('users', values)  
-            data = True
+            try:
+                dB.insert_into('users', values)  
+            except Error as e:
+                print(e)
             if payload['email'] not in session:
                     session['email'] = payload['email']
                     return redirect(url_for('home'))
@@ -53,11 +59,10 @@ def Hello_World():
 
 @myapp.route('/home')
 def home():
-    
     user = session['email']
     return render_template('home.html',user = user, login_status = True)
 
 
 if __name__ == "__main__":
-    myapp.run(debug = True)
+    myapp.run(debug=True)
 
