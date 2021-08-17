9750a9787d0140c451b270ab342c26ed327833bb
Ashutosh Muley <muleyashutosh@gmail.com>
2021-08-17 15:21:53 +0530

    responsive layout for homePageNav

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,118 +1,125 @@
-NProgress.configure({ showSpinner: false })
-
+NProgress.configure({ showSpinner: false });
+debugger;
 const { MDCRipple } = mdc.ripple;
 const { MDCTooltip } = mdc.tooltip;
 
 const { MDCDialog } = mdc.dialog;
 
-const dialogEl = document.querySelector('.mdc-dialog')
+const dialogEl = document.querySelector(".mdc-dialog");
 const dialog = new MDCDialog(dialogEl);
 
-const tooltips = [].map.call(document.querySelectorAll('.mdc-tooltip'), el => {
+const tooltips = [].map.call(
+  document.querySelectorAll(".mdc-tooltip"),
+  (el) => {
     return new MDCTooltip(el);
+  }
+);
+
+const iconRipples = [].map.call(
+  document.querySelectorAll(".mdc-icon-button"),
+  (el) => {
+    return new MDCRipple(el);
+  }
+);
+
+iconRipples.map((el) => {
+  el.unbounded = true;
 });
 
-
-const iconRipples = [].map.call(document.querySelectorAll('.mdc-icon-button'), el => {
-    return new MDCRipple(el);
-})
-
-iconRipples.map(el => {
-    el.unbounded = true;
-})
-
+console.log(iconRipples);
 
 const tooltipsCleanup = () => {
-    tooltips.map(({ root }) => {
-        if (root.classList.contains('mdc-tooltip--showing') || root.classList.contains('mdc-tooltip--hide')) {
-            root.classList.remove('mdc-tooltip--showing')
-            root.classList.remove('mdc-tooltip--hide')
-        }
-    })
-}
+  tooltips.map(({ root }) => {
+    if (
+      root.classList.contains("mdc-tooltip--showing") ||
+      root.classList.contains("mdc-tooltip--hide")
+    ) {
+      root.classList.remove("mdc-tooltip--showing");
+      root.classList.remove("mdc-tooltip--hide");
+    }
+  });
+};
 
-setInterval(tooltipsCleanup, 25)
-
+setInterval(tooltipsCleanup, 25);
 
 $(() => {
-    const LOGO = `/static/styles/logo.png`
-    const SHORT_LOGO = `/static/styles/logo1.png`
-    const adjustLogo = () => {
-        const width = window.innerWidth;
-        const img = document.getElementById('headerLogo')
-        if(width < 467) {
-            img.src = SHORT_LOGO;
-        } else {
-            img.src = LOGO;
-        }
+  const LOGO = `/static/styles/logo.png`;
+  const SHORT_LOGO = `/static/styles/logo1.png`;
+
+  const adjustLogo = () => {
+    const width = window.outerWidth;
+    const img = document.getElementById("headerLogo");
+    if (width < 467) {
+      img.src = SHORT_LOGO;
+    } else {
+      img.src = LOGO;
     }
+  };
 
-    adjustLogo()
+  adjustLogo();
 
-    $(window).resize(adjustLogo)
+  $(window).resize(adjustLogo);
 
-    const floatingSearchToggle = () => {
-        const searchfield = $('#searchfield')
-        // console.log(searchfield.hasClass('floating-search'))
-        if (searchfield.hasClass('floating-search')) {
-            searchfield.slideUp(100)
-            console.log(searchfield.attr('style'))
-        }
-        else {
-            searchfield.slideDown(50).css('display', 'flex')
-        }
-        searchfield.toggleClass('floating-search')
+  const floatingSearchToggle = () => {
+    const searchfield = $("#searchfield");
+    // console.log(searchfield.hasClass('floating-search'))
+    if (searchfield.hasClass("floating-search")) {
+      searchfield.slideUp(100);
+      console.log(searchfield.attr("style"));
+      searchfield.attr("style", "");
+    } else {
+      searchfield.slideDown(50).css("display", "flex");
     }
+    searchfield.toggleClass("floating-search");
+  };
 
-    $('.search-icon-button').on('click', floatingSearchToggle)
-    
-    $('#dropbtn').click(function() {
-        $('#drop-content').slideToggle(200);    
-    })
+  $(".search-icon-button").on("click", floatingSearchToggle);
 
-    $('#search').focusin(function() {
-        $('#category').css("border", '2.5px solid #fdc100')
-        $('#category').css("border-right", 'none')
-        $('#search').css('border-top', '2.5px solid #fdc100')
-        $('#search').css('border-bottom', '2.5px solid #fdc100')
-    })
-    $('#search').focusout(function() {
-        $('#category').css("border", 'none')
-        $('#search').css('border', 'none')
-    })
-    $('#category').focusin(function() {
-        $('#search').css('border', '2.5px solid #fdc100')
-        $('#search').css('border-left', 'none')
-        $('#search').css('border-right', 'none')
-        $('#category').css("border", '2.5px solid #fdc100')
-        $('#category').css("border-right", 'none')
-    })
+  $("#dropbtn").click(function () {
+    $("#drop-content").slideToggle(200);
+  });
 
-    window.onclick = function(e) {
-        if (!e.target.matches('#dropbtn') && !e.target.matches('#drop-btn')) {
-            $('#drop-content').slideUp();
-        }
-        
+  $("#search").focusin(function () {
+    $("#category").css("border", "2.5px solid #fdc100");
+    $("#category").css("border-right", "none");
+    $("#search").css("border-top", "2.5px solid #fdc100");
+    $("#search").css("border-bottom", "2.5px solid #fdc100");
+  });
+  $("#search").focusout(function () {
+    $("#category").css("border", "none");
+    $("#search").css("border", "none");
+  });
+  $("#category").focusin(function () {
+    $("#search").css("border", "2.5px solid #fdc100");
+    $("#search").css("border-left", "none");
+    $("#search").css("border-right", "none");
+    $("#category").css("border", "2.5px solid #fdc100");
+    $("#category").css("border-right", "none");
+  });
+
+  window.onclick = function (e) {
+    if (!e.target.matches("#dropbtn") && !e.target.matches("#drop-btn")) {
+      $("#drop-content").slideUp();
     }
+  };
 
-    $('#category').change(function() {
-        // console.log('resizing')
-        $('#resizerOption').html($('#category option:selected').text())
-        // console.log($('#resizerOption').width())
-        $('#category').width($('#resizerSelect').width())
-    })
+  $("#category").change(function () {
+    // console.log('resizing')
+    $("#resizerOption").html($("#category option:selected").text());
+    // console.log($('#resizerOption').width())
+    $("#category").width($("#resizerSelect").width());
+  });
 
-    $('.logout').click(() => {
-        dialog.open()
-    })
+  $(".logout").click(() => {
+    dialog.open();
+  });
 
-    $('.logout_button').click(() => {
-        const a = document.createElement('a');
-        a.href = '/logout';
-        a.style = "display: none"
-        document.body.appendChild(a)
-        a.click();
-    })
-
-
-})
+  $(".logout_button").click(() => {
+    const a = document.createElement("a");
+    a.href = "/logout";
+    a.style = "display: none";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+});
--- static/scripts/customer-home.js
+++ static/scripts/customer-home.js
@@ -4,7 +4,7 @@ const { MDCRadio } = mdc.radio;
 const { MDCList } = mdc.list;
 const { MDCDrawer } = mdc.drawer;
 
-const selector = ".mdc-button, .mdc-icon-button, .mdc-card__primary-action";
+const selector = ".mdc-card__primary-action";
 const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
   return new MDCRipple(el);
 });
@@ -24,132 +24,117 @@ const filterRipples = [].map.call(
   }
 );
 
-$(document).ready(function () {
-  const checkboxes = [].map.call(
-    document.querySelectorAll(".mdc-checkbox"),
-    (el) => {
-      return new MDCCheckbox(el);
-    }
-  );
-
-  const formFieldsCheckboxes = [].map.call(
-    document.querySelectorAll(".mdc-form-field-checkboxes"),
-    (el) => {
-      return new MDCFormField(el);
-    }
-  );
-
-  formFieldsCheckboxes.map((el, index) => {
-    el.input = checkboxes[index];
-  });
-
-  const radios = [].map.call(document.querySelectorAll(".mdc-radio"), (el) => {
-    return new MDCRadio(el);
-  });
-
-  const formFieldsRadios = [].map.call(
-    document.querySelectorAll(".mdc-form-field-radios"),
-    (el) => {
-      return new MDCFormField(el);
-    }
-  );
-
-  formFieldsRadios.map((el, index) => {
-    el.input = radios[index];
-  });
-
-  document.getElementById("reset").addEventListener("click", function (event) {
-    document.getElementById("filter").reset();
-    return false;
-  });
-
-  const filterRipples = [].map.call(
-    document.querySelectorAll(".filter-button"),
-    (el) => {
-      new MDCRipple(el);
-    }
-  );
-
-  const initialize_button_Ripples = () => {
-    const ripples = [].map.call(
-      document.querySelectorAll(".addToCart"),
-      (el) => {
-        new MDCRipple(el);
-      }
-    );
-  };
-  initialize_button_Ripples();
-
-  pages = $(".pageswitcher").children("a");
-  dot_count = 0;
-  for (x of pages) {
-    if (x.text == "·") {
-      dot_count++;
-      x.style.display = "none";
-    }
+const checkboxes = [].map.call(
+  document.querySelectorAll(".mdc-checkbox"),
+  (el) => {
+    return new MDCCheckbox(el);
   }
-  dots = 0;
-  for (x of pages) {
-    if (x.text == "·" && dots < 5) {
-      x.style.display = "";
-      dots++;
-    }
-  }
-  dots = 0;
-  for (x of pages.get().reverse()) {
-    if (x.text == "·" && dots < 5) {
-      x.style.display = "";
-      dots++;
-    }
-  }
-  // console.log(pages.get().reverse())
+);
 
-  const QuerySearch = (query, category, callback) => {
-    $.ajax({
-      method: "post",
-      url: "/api/getproducts",
-      data: {
-        search: query,
-        category: category,
-      },
-      success: callback,
-    });
-  };
-  const gridContainer = $(".grid-container");
-  var allproducts = gridContainer.contents();
-  const searchInput = $("#search");
-  const searchButton = $("#search-icon");
-  // console.log(allproducts)
+const formFieldsCheckboxes = [].map.call(
+  document.querySelectorAll(".mdc-form-field-checkboxes"),
+  (el) => {
+    return new MDCFormField(el);
+  }
+);
 
-  const renderProducts = ({ status, data }) => {
-    // console.log(re111s)
-    if (status !== "OK") return;
-    NProgress.done();
-    if (data.length == 0) {
-      // console.log('val ' + $("#search").val().trim() + ', data empty')
-      var error =
-        '<div class="noItemsFound"><h2>Oops, No Items matching your search were found!</h2><hr></div>';
-      if (!$(".main-content").has(".noItemsFound").length)
-        $(".main-content").append(error);
-      $(".grid-container").empty();
-    } else {
-      // console.log('val ' + $("#search").val().trim() + ', res not empty')
-      const html = data.reduce((prev, item) => {
-        // return `${prev}<div class='grid-item mdc-elevation--z2' id='${item["prodID"]}'>
-        //                     <img src="${item["img"]}" alt="">
-        //                     <div>${item["prodName"]}</div>
-        //                     <div class='priceTag'>&#x20B9; ${item["minPrice"]}.00</div>
-        //                     <span class="addCartButtonContainer">
-        //                         <button class="addToCart mdc-button mdc-button--raised">
-        //                             <span class="mdc-button__ripple"></span>
-        //                             <i class="material-icons mdc-button__icon" aria-hidden="true">
-        //                                 add_shopping_cart
-        //                             </i>
-        //                             <span class="mdc-button__label">Add to Cart</span>
-        //                         </button>
-        //                     </span>
-        //                 </div>`;
-        return `${prev}<div class="mdc-card">
+formFieldsCheckboxes.map((el, index) => {
+  el.input = checkboxes[index];
+});
+
+const radios = [].map.call(document.querySelectorAll(".mdc-radio"), (el) => {
+  return new MDCRadio(el);
+});
+
+const formFieldsRadios = [].map.call(
+  document.querySelectorAll(".mdc-form-field-radios"),
+  (el) => {
+    return new MDCFormField(el);
+  }
+);
+
+formFieldsRadios.map((el, index) => {
+  el.input = radios[index];
+});
+
+// document.getElementById("reset").addEventListener("click", function (event) {
+//   document.getElementById("filter").reset();
+//   return false;
+// });
+
+const filterRipples = [].map.call(
+  document.querySelectorAll(".filter-button"),
+  (el) => {
+    new MDCRipple(el);
+  }
+);
+
+// const initialize_button_Ripples = () => {
+//   const ripples = [].map.call(
+//     document.querySelectorAll(".addToCart"),
+//     (el) => {
+//       new MDCRipple(el);
+//     }
+//   );
+// };
+// initialize_button_Ripples();
+
+pages = $(".pageswitcher").children("a");
+dot_count = 0;
+for (x of pages) {
+  if (x.text == "·") {
+    dot_count++;
+    x.style.display = "none";
+  }
+}
+dots = 0;
+for (x of pages) {
+  if (x.text == "·" && dots < 5) {
+    x.style.display = "";
+    dots++;
+  }
+}
+dots = 0;
+for (x of pages.get().reverse()) {
+  if (x.text == "·" && dots < 5) {
+    x.style.display = "";
+    dots++;
+  }
+}
+// console.log(pages.get().reverse())
+
+const QuerySearch = (query, category, callback) => {
+  $.ajax({
+    method: "post",
+    url: "/api/getproducts",
+    data: {
+      search: query,
+      category: category,
+    },
+    success: callback,
+  });
+};
+const gridContainer = $(".grid-container");
+var allproducts = gridContainer.contents();
+const searchInput = $("#search");
+const searchButton = $("#search-icon");
+// console.log(allproducts)
+
+const renderProducts = ({ status, data }) => {
+  // console.log(re111s)
+  if (status !== "OK") return;
+  NProgress.done();
+  if (data.length == 0) {
+    // console.log('val ' + $("#search").val().trim() + ', data empty')
+    var error =
+      '<div class="noItemsFound"><h2>Oops, No Items matching your search were found!</h2><hr></div>';
+    if (!$(".main-content").has(".noItemsFound").length)
+      $(".main-content").append(error);
+    $(".grid-container").empty();
+  } else {
+    // console.log('val ' + $("#search").val().trim() + ', res not empty')
+    const html = data.reduce((prev, item) => {
+      return `${prev}<div class="mdc-card">
         <div class="mdc-card__primary-action">
           <div
             class="mdc-card__media mdc-card__media--square"
@@ -179,72 +164,72 @@ $(document).ready(function () {
           </div>
         </div>
       </div>`;
-      }, "");
-      $(".noItemsFound").remove();
+    }, "");
+    $(".noItemsFound").remove();
+    gridContainer.empty();
+    gridContainer.append(html);
+    $(".pageswitcher").hide();
+
+    initialize_button_Ripples();
+  }
+};
+
+searchButton.click(function () {
+  if (searchInput.val().trim() == "") {
+    if (allproducts.length == 0) {
+      // console.log('val empty, productslist empty')
+      var error =
+        '<div class="noItemsFound"><h2>Oops, No Items matching your search were found!</h2><hr></div>';
       gridContainer.empty();
-      gridContainer.append(html);
-      $(".pageswitcher").hide();
-
-      initialize_button_Ripples();
-    }
-  };
-
-  searchButton.click(function () {
-    if (searchInput.val().trim() == "") {
-      if (allproducts.length == 0) {
-        // console.log('val empty, productslist empty')
-        var error =
-          '<div class="noItemsFound"><h2>Oops, No Items matching your search were found!</h2><hr></div>';
-        gridContainer.empty();
-        $(".main-content").append(error);
-      } else {
-        // console.log('val empty, productslist not empty')
-        $(".noItemsFound").remove();
-        if (!gridContainer.contents().length) gridContainer.append(allproducts);
-        $(".pageswitcher").slideDown();
-      }
+      $(".main-content").append(error);
     } else {
-      NProgress.start();
-      QuerySearch(
-        searchInput.val().trim(),
-        $("#category").val(),
-        renderProducts
-      );
+      // console.log('val empty, productslist not empty')
+      $(".noItemsFound").remove();
+      if (!gridContainer.contents().length) gridContainer.append(allproducts);
+      $(".pageswitcher").slideDown();
     }
-  });
-
-  searchInput.keypress(function (e) {
-    if (e.keyCode == 13) {
-      $("#search-icon").click();
-    }
-  });
-
-  const fetchFilteredProducts = async (body) => {
-    const resp = await fetch("/api/filter", {
-      headers: { "Content-Type": "application/json" },
-      method: "post",
-      body: body,
-    });
-    const data = resp.json();
-    return data;
-  };
-
-  const filterForm = document.querySelector("#filter");
-  filterForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+  } else {
     NProgress.start();
-    const formData = new FormData(filterForm);
-    const body = JSON.stringify(Object.fromEntries(formData));
-    fetchFilteredProducts(body).then((data) => {
-      renderProducts(data);
-    });
-  });
-
-  const redirectToProductPage = (e) => {
-    const url = e.target.getAttribute("data-redirect");
-    // console.log(url);
-    window.location = url;
-  };
-
-  $(".mdc-card__primary-action").on("click", redirectToProductPage);
+    QuerySearch(searchInput.val().trim(), $("#category").val(), renderProducts);
+  }
 });
+
+searchInput.keypress(function (e) {
+  if (e.keyCode == 13) {
+    $("#search-icon").click();
+  }
+});
+
+const fetchFilteredProducts = async (body) => {
+  const resp = await fetch("/api/filter", {
+    headers: { "Content-Type": "application/json" },
+    method: "post",
+    body: body,
+  });
+  const data = resp.json();
+  return data;
+};
+
+const filterForm = document.querySelector("#filter");
+filterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  NProgress.start();
+  const formData = new FormData(filterForm);
+  const body = JSON.stringify(Object.fromEntries(formData));
+  fetchFilteredProducts(body).then((data) => {
+    renderProducts(data);
+  });
+});
+
+const redirectToProductPage = (e) => {
+  const url = e.target.getAttribute("data-redirect");
+  // console.log(url);
+  const a = document.createElement("a");
+  a.href = url;
+  a.style = "display: none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
+
+$(".mdc-card__primary-action").on("click", redirectToProductPage);
