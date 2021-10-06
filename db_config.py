afbf49377f124262205983279a17aa1bd55286be
Ashutosh Muley <muleyashutosh@gmail.com>
2021-10-06 23:03:49 +0530

    .env update

--- db_config.py
+++ db_config.py
@@ -10,13 +10,3 @@ CONFIG = {
     "password": os.getenv('mysql-password'),
     "database": os.getenv('mysql-database')
 }
-
-# print(CONFIG)
-
-# mysql-database = "b8aglplfcrplneriinhm"
-# mysql-host = "b8aglplfcrplneriinhm-mysql.services.clever-cloud.com"
-# mysql-password = "5xNfpljjVoU0c6z32ljA"
-# mysql-port = "3306"
-# MYSQL_ADDON_URI = "mysql://uygnkepsgtpfxcep:5xNfpljjVoU0c6z32ljA@b8aglplfcrplneriinhm-mysql.services.clever-cloud.com:3306/b8aglplfcrplneriinhm"
-# mysql-user = "uygnkepsgtpfxcep"
-# MYSQL_ADDON_VERSION = "8.0"
